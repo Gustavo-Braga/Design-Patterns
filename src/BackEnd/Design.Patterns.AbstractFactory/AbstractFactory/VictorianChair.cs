71476cd75de36cdbdd2091de6e856cc2c91dfcba
Braga <gustavo.braga10@outlook.com>
2020-05-02 16:26:36 -0300

    Criado patter abstract factory

---
+++ src/BackEnd/Design.Patterns.AbstractFactory/AbstractFactory/VictorianChair.cs
@@ -0,0 +1,25 @@
+﻿using Design.Patterns.AbstractFactory.Interfaces;
+
+namespace Design.Patterns.AbstractFactory.AbstractFactory
+{
+    public class VictorianChair : IChair
+    {
+        public VictorianChair()
+        {
+        }
+
+        public VictorianChair(string texture, bool isMetallic)
+        {
+            Texture = texture;
+            IsMetallic = isMetallic;
+        }
+
+        public string Texture { get; set; }
+        public bool IsMetallic { get; set; }
+
+        public string GetType()
+        {
+            return $"Esta é uma cadeira Vitoriana de textura {(string.IsNullOrEmpty(Texture)? "tecido": Texture)} e com acabamento {(IsMetallic? "metálico": "de madeira")}";
+        }
+    }
+}
---
+++ src/BackEnd/Design.Patterns.AbstractFactory/Interfaces/IChair.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design.Patterns.AbstractFactory.Interfaces
+{
+    public interface IChair
+    {
+        string GetType();
+    }
+}
