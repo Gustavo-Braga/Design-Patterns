54c07275c9415990c2ce063c8348b452a06c317b
Braga <gustavo.braga10@outlook.com>
2020-05-03 23:45:31 -0300

    Implementado singleton pattern

---
+++ src/BackEnd/Design.Patterns.Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using Design.Patterns.Singleton.Repository;
+using System;
+
+namespace Design.Patterns.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+            var repository = ProductRepository.GetInstance("Product");
+
+            Console.ReadKey();
+        }
+    }
+}
