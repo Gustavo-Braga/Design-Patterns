fcebf607ac51034f8039b0186ab184cd85d177a3
Gustavo Braga <gustavo.braga10@outlook.com>
2020-05-05 23:05:38 -0300

    Implementa padrão Prototype

---
+++ src/BackEnd/Design.Patterns.Prototype/Model/QAAnalyst.cs
@@ -0,0 +1,29 @@
+﻿using Design.Patterns.Prototype.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Design.Patterns.Prototype.Model
+{
+    public class QAAnalyst : IEmployee, ICloneable
+    {
+        public string Name { get; set; }
+        private double Salary { get; set; }
+        public IEnumerable<string> Frameworks{ get; set; }
+
+        public IEmployee CloneEmployee()
+        {
+            return (QAAnalyst)MemberwiseClone();
+        }
+
+        object ICloneable.Clone()
+        {
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Salary}, {string.Join(", ", Frameworks)}";
+        }
+    }
+}
---
+++ src/BackEnd/Design.Patterns.Prototype/Program.cs
@@ -0,0 +1,29 @@
+﻿using Design.Patterns.Prototype.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Design.Patterns.Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var developer = new Developer("Fulano", new Random().NextDouble(), new List<string> { "C#","JS" });
+            var prototype = developer.CloneEmployee();
+            Console.WriteLine(prototype.ToString());
+            developer.Name = "Cicrano";
+            Console.WriteLine(prototype.ToString());
+
+
+            var prototype2 = (Developer)developer.Clone();
+            Console.WriteLine(prototype2.ToString());
+            prototype2.Name = "Beltrano";
+            Console.WriteLine(prototype2.ToString());
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
