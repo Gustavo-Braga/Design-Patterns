c7b14e5d52964ab3318b6cfcbcd4861fcce589e8
Gustavo Braga <gustavo.braga10@outlook.com>
2020-05-10 16:05:35 -0300

    Implementado pattern strutural adapter

---
+++ src/BackEnd/Design.Patterns.Adapter/Adapter/EmailAdapter.cs
@@ -0,0 +1,15 @@
+﻿using Design.Patterns.Adapter.Client.Request;
+using Design.Patterns.Adapter.Interfaces;
+using Design.Patterns.Adapter.Model;
+using Newtonsoft.Json;
+
+namespace Design.Patterns.Adapter.Adapter
+{
+    public class EmailAdapter: IEmailAdapter
+    {
+        public EmailClientRequest GetEmailRequest(Email emailRequest)
+        {
+            return new EmailClientRequest(emailRequest.Address, JsonConvert.SerializeObject(emailRequest.Body));
+        }
+    }
+}
---
+++ src/BackEnd/Design.Patterns.Adapter/Model/Email.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design.Patterns.Adapter.Model
+{
+    public class Email
+    {
+        public Email(string address, BodyEmail body)
+        {
+            Address = address;
+            Body = body;
+        }
+
+        public string Address { get; set; }
+        public BodyEmail Body { get; set; }
+
+    }
+    public class BodyEmail
+    {
+        public BodyEmail(string subject, string body)
+        {
+            Subject = subject;
+            Body = body;
+        }
+
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+    }
+}
