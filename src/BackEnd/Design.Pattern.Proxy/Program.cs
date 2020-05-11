4fe304b81d3eb1aea8dab7d36edd6f1185368ebc
Gustavo Braga <gustavo.braga10@outlook.com>
2020-05-10 21:37:49 -0300

    Criado pattern Proxy

---
+++ src/BackEnd/Design.Pattern.Proxy/Program.cs
@@ -0,0 +1,26 @@
+﻿using Design.Pattern.Proxy.Model;
+using Design.Pattern.Proxy.Proxy;
+using Design.Pattern.Proxy.Repository;
+using System;
+
+namespace Design.Pattern.Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var product1 = new Product("Produto 1");
+
+            var productRepository = new ProductRepository();
+            productRepository.Insert(product1);
+
+            var product2 = new Product("Produto 2");
+            var proxyProductRepository = new ProxyProductRepository();
+            proxyProductRepository.Insert(product2);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ src/BackEnd/Design.Pattern.Proxy/Repository/ProductLogRepository.cs
@@ -0,0 +1,15 @@
+﻿using Design.Pattern.Proxy.Interfaces;
+using Design.Pattern.Proxy.Model;
+using System;
+
+namespace Design.Pattern.Proxy.Repository
+{
+    public class ProductLogRepository : IProductLogRepository
+    {
+        public int Insert(Product product)
+        {
+            Console.WriteLine($"Produto Log inserido = id: {product.Id}, name: {product.Name}");
+            return product.Id;
+        }
+    }
+}
