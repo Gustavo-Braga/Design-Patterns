fe601449330f130006749a33b0e7bddbbc8b44e3
Gustavo Braga <gustavo.braga10@outlook.com>
2020-05-12 22:54:05 -0300

    Cria estrutura inicial para o pattern flyweight

---
+++ src/BackEnd/Design.Pattern.Flyweight/Flyweight/VehicleFlyweight.cs
@@ -0,0 +1,34 @@
+﻿using Design.Pattern.Flyweight.Model;
+using System.Collections.Generic;
+
+namespace Design.Pattern.Flyweight.Flyweight
+{
+    public class VehicleFlyweight
+    {
+        public VehicleFlyweight(Dictionary<string, Truck> trucks)
+        {
+            Trucks = trucks;
+        }
+
+        private Dictionary<string, Truck> Trucks { get; set; }
+
+        public Truck GetVehicles(string key)
+        {
+            if (Trucks.ContainsKey(key))
+                return Trucks[key];
+            else
+            {
+                Trucks.Add(key, new Truck());
+                return Trucks[key];
+            }
+        }
+
+        public void GetListTrucks()
+        {
+            foreach (var item in Trucks.Values)
+            {
+                item.Show();
+            }
+        }
+    }
+}
---
+++ src/BackEnd/Design.Pattern.Flyweight/Program.cs
@@ -0,0 +1,18 @@
+﻿using Design.Pattern.Flyweight.Model;
+using System;
+
+namespace Design.Pattern.Flyweight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            Console.ReadKey();
+        }
+
+     }
+}
