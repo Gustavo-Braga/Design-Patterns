abf6adeae311ab92a08d4d4f3520b06f9ec76b53
Gustavo Braga <5by5.gustavo@voeazul.com.br>
2020-05-15 17:05:59 -0300

    implementado pattern Memento

---
+++ src/BackEnd/Design.Patterns.Memento/Interfaces/IMemento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Design.Patterns.Memento.Interfaces
+{
+    public interface IMemento
+    {
+        IOriginator GetState();
+        DateTime GetDate();
+        void Show();
+    }
+}
---
+++ src/BackEnd/Design.Patterns.Memento/Memento/Caretaker.cs
@@ -0,0 +1,35 @@
+﻿using Design.Patterns.Memento.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Design.Patterns.Memento.Memento
+{
+    public class Caretaker
+    {
+        private Stack<IMemento> _stack = new Stack<IMemento>();
+        private IOriginator _originator;
+        public Caretaker(IOriginator originator)
+        {
+            _originator = originator;
+        }
+
+        public void Backup()
+        {
+            _stack.Push(_originator.Save());
+        }
+
+        public void Undo()
+        {
+            _stack.Pop();
+        }
+
+        public void ShowHistory()
+        {
+            foreach (var item in _stack)
+            {
+                Console.WriteLine($"{item.GetDate().ToString("dd/MM/yyyy HH:mm:ss")}");
+                item.Show();
+            }
+        }
+    }
+}
