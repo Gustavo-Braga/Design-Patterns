b4aaf869e0d8e9872756fe22c1cf62f957935579
Gustavo Braga <gustavo.braga10@outlook.com>
2020-05-18 16:06:05 -0300

    Criado pattern template method

---
+++ src/BackEnd/Design.Pattern.TemplateMethod/File/CsvFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Design.Pattern.TemplateMethod.File
+{
+    public class CsvFile : FileBase
+    {
+
+        public override void OpenFile(string directory)
+        {
+            Console.WriteLine($"Abre arquivo CSV no diretório ~~ {directory}");
+        }
+
+        public override void CloseFile(string directory)
+        {
+            Console.WriteLine($"Fecha arquivo CSV no diretório ~~ {directory}");
+        }
+
+        public override IEnumerable<string> ExtractData()
+        {
+            return new List<string>()
+            {
+                "A,B,C",
+                "1,2,3",
+                "1,2,3",
+                "1,2,3",
+                "1,2,3",
+                "1,2,3"
+            };
+        }
+    }
+}
---
+++ src/BackEnd/Design.Pattern.TemplateMethod/File/PdfFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Design.Pattern.TemplateMethod.File
+{
+    public class PdfFile : FileBase
+    {
+
+        public override void OpenFile(string directory)
+        {
+            Console.WriteLine($"Abre arquivo PDF no diretório ~~ {directory}");
+        }
+
+        public override void CloseFile(string directory)
+        {
+            Console.WriteLine($"Fecha arquivo PDF no diretório ~~ {directory}");
+        }
+
+    }
+}
